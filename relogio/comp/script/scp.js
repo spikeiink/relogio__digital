edcfff5c0d2a7206357c94c9fe7477cf21ecb87c
Douglas \Maziero <promax.contato@hotmail.com>
2022-10-11 23:12:48 -0300

    Add files via upload

---
+++ relogio/comp/script/scp.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+
+const relogio = setInterval(function time(){
+    let ndate = new Date();
+    let hr = ndate.getHours();
+    let min = ndate.getMinutes();
+    let seg = ndate.getSeconds();
+
+    if(hr <10)hr = '0' + hr;
+    if(min <10)min = '0' + min;
+    if(seg<10)seg = '0' + seg;
+
+
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent= seg;
+})
